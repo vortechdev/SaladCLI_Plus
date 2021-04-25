9c3232f15da6d5add3779ee9e8471dd151d31433
Walkx <71191962+WalkxCode@users.noreply.github.com>
2021-04-25 23:45:37 +0200

    Update SETUP.py

--- Salad CLI/SETUP.py
+++ Salad CLI/SETUP.py
@@ -4,6 +4,7 @@ import os
 sys.stdout.write("\x1b]2;Salad CLI+ SETUP\x07")
 os.system('python -m pip install --upgrade "pip @ https://github.com/uranusjr/pip/archive/refs/heads/sysconfig-header-with-none-project.zip"')
 os.system("python -m pip install python-dateutil")
+os.system("python -m pip install pyperclip")
 os.system("python -m pip install requests")
 os.system("pause")
 print("Setup finished")
