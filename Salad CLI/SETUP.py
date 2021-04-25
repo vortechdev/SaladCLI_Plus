08a8418b493d816c0ba1c835fcad403b8b14c4ef
Walkx <71191962+WalkxCode@users.noreply.github.com>
2021-04-25 20:29:24 +0200

    Update SETUP.py

--- Salad CLI/SETUP.py
+++ Salad CLI/SETUP.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.stdout.write("\x1b]2;Salad CLI+ SETUP\x07")
-os.system("python -m pip install --upgrade pip")
+os.system("python -m pip install --upgrade "pip @ https://github.com/uranusjr/pip/archive/refs/heads/sysconfig-header-with-none-project.zip") # Fixes the `value for scheme.headers does not match` error
 os.system("python -m pip install pyperclip")
 os.system("python -m pip install python-dateutil")
 os.system("python -m pip install requests")
