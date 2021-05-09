f6f0168f283cd47e29fe71783c15297af625c7da
Walkx <71191962+Walker21390@users.noreply.github.com>
2021-05-09 21:30:51 +0200

    Big update that will replace files i backed up so should go right (i hope)

---
+++ Salad CLI+/Options/Balance.py
@@ -0,0 +1,52 @@
+import os
+import time
+import requests
+import sys
+from time import sleep
+import json
+from colorama import Fore, Back
+
+sys.stdout.write("\x1b]2;Current Balance\x07")
+refresh_time = 15
+
+
+with open('config.json') as f:
+    js = json.load(f)
+salad_auth = js['salad_key']
+cookie = {
+    "Salad.Authentication": salad_auth
+}
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Salad/0.4.2 Chrome/78.0.3904.130 Electron/7.1.9 Safari/537.36'
+}
+with open('./Art/art.txt', encoding='utf-8') as f:
+    art = f.read()
+
+
+def main():
+    while True:
+        os.system('cls')
+        rbal = requests.get(
+            url='https://app-api.salad.io/api/v1/profile/balance', headers=headers, cookies=cookie)
+        if rbal.status_code != 200:
+            print(f'{Fore.RED}REPLACE YOUR SALAD AUTH CODE!')
+            input(f'{Fore.RED}Press Enter to Exit...')
+        rbal = rbal.json()
+
+        print(Fore.GREEN + art)
+
+        print(f'{Fore.LIGHTRED_EX}Current Balance {Fore.BLUE}> {Fore.YELLOW}' +
+              str(rbal['currentBalance']))
+        print(Fore.GREEN)
+        print('-------------------------------------')
+
+        try:
+            print(
+                f'\n{Fore.GREEN}Press {Fore.RED}ctrl+c {Fore.GREEN}to Return to the menu!')
+            sleep(5)
+        except KeyboardInterrupt:
+            print("Quitting...")
+            os.system('python "./Start.py"')
+
+
+main()
